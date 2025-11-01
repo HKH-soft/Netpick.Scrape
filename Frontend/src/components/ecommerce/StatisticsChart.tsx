d17bc212d65076abb2765d86a87dfc6a63c818c6
Hossein <hkh.soft.eng@gmail.com>
2025-11-01 14:06:54 +0330

    changed the color of from blue to green

--- Frontend/src/components/ecommerce/StatisticsChart.tsx
+++ Frontend/src/components/ecommerce/StatisticsChart.tsx
@@ -17,7 +17,7 @@ export default function StatisticsChart() {
       position: "top",
       horizontalAlign: "left",
     },
-    colors: ["#465FFF", "#9CB9FF"], // Define line colors
+    colors: ["#009407", "#60ff65"], // Define line colors
     chart: {
       fontFamily: "Outfit, sans-serif",
       height: 310,
--- Frontend/src/components/form/switch/Switch.tsx
+++ Frontend/src/components/form/switch/Switch.tsx
@@ -6,7 +6,7 @@ interface SwitchProps {
   defaultChecked?: boolean;
   disabled?: boolean;
   onChange?: (checked: boolean) => void;
-  color?: "blue" | "gray"; // Added prop to toggle color theme
+  color?: "brand" | "gray"; // Added prop to toggle color theme
 }
 
 const Switch: React.FC<SwitchProps> = ({
@@ -14,7 +14,7 @@ const Switch: React.FC<SwitchProps> = ({
   defaultChecked = false,
   disabled = false,
   onChange,
-  color = "blue", // Default to blue color
+  color = "brand", // Default to brand color
 }) => {
   const [isChecked, setIsChecked] = useState(defaultChecked);
 
@@ -28,38 +28,36 @@ const Switch: React.FC<SwitchProps> = ({
   };
 
   const switchColors =
-    color === "blue"
+    color === "brand"
       ? {
-          background: isChecked
-            ? "bg-brand-500 "
-            : "bg-gray-200 dark:bg-white/10", // Blue version
-          knob: isChecked
-            ? "translate-x-full bg-white"
-            : "translate-x-0 bg-white",
-        }
+        background: isChecked
+          ? "bg-brand-500 "
+          : "bg-gray-200 dark:bg-white/10", // brand version
+        knob: isChecked
+          ? "translate-x-full bg-white"
+          : "translate-x-0 bg-white",
+      }
       : {
-          background: isChecked
-            ? "bg-gray-800 dark:bg-white/10"
-            : "bg-gray-200 dark:bg-white/10", // Gray version
-          knob: isChecked
-            ? "translate-x-full bg-white"
-            : "translate-x-0 bg-white",
-        };
+        background: isChecked
+          ? "bg-gray-800 dark:bg-white/10"
+          : "bg-gray-200 dark:bg-white/10", // Gray version
+        knob: isChecked
+          ? "translate-x-full bg-white"
+          : "translate-x-0 bg-white",
+      };
 
   return (
     <label
-      className={`flex cursor-pointer select-none items-center gap-3 text-sm font-medium ${
-        disabled ? "text-gray-400" : "text-gray-700 dark:text-gray-400"
-      }`}
+      className={`flex cursor-pointer select-none items-center gap-3 text-sm font-medium ${disabled ? "text-gray-400" : "text-gray-700 dark:text-gray-400"
+        }`}
       onClick={handleToggle} // Toggle when the label itself is clicked
     >
       <div className="relative">
         <div
-          className={`block transition duration-150 ease-linear h-6 w-11 rounded-full ${
-            disabled
+          className={`block transition duration-150 ease-linear h-6 w-11 rounded-full ${disabled
               ? "bg-gray-100 pointer-events-none dark:bg-gray-800"
               : switchColors.background
-          }`}
+            }`}
         ></div>
         <div
           className={`absolute left-0.5 top-0.5 h-5 w-5 rounded-full shadow-theme-sm duration-150 ease-linear transform ${switchColors.knob}`}
